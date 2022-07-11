2946810164663ea0caad5edab40cecf525eef169
ju8ko <julia.x.kobzar@gmail.com>
2022-07-11 15:47:58 +0200

    The exercie is ready

---
+++ solution.js
@@ -0,0 +1,27 @@
+//1
+const isDog = false;
+console.log(isDog === true ? "pat, pat" : "find me a dog to pat!");
+//2
+const speedLimit = 50;
+const yourSpeed = 70;
+console.log(
+  yourSpeed > 50
+    ? "you're going too fast!"
+    : "You're driving below the speed limit"
+);
+//3
+const age = 43;
+console.log(age < 16 ? "serve butter beer" : "serve beer");
+//4
+isStudent = false;
+console.log(isStudent === true ? "Ticket costs €5,00" : "Ticket costs €12,00");
+//5
+const okMarie = "cake";
+console.log(okMarie === "cake" ? "Let them eat cake" : "Oh, bother");
+//6
+const num1 = 30;
+const num2 = 99;
+const num3 = 31;
+console.log(num1 % 2 === 0 ? `${num1} is even` : `${num1} is odd`);
+console.log(num2 % 2 === 0 ? `${num2} is even` : `${num2} is odd`);
+console.log(num3 % 2 === 0 ? `${num3} is even` : `${num3} is odd`);
